378f20dd305f6da0e7785baeeb2369bff72164a2
7kHz <guitarheroe2012@ya.ru>
2023-05-16 23:10:54 +0300

    add models_save

--- 2.1-databases/work_with_database/phones/admin.py
+++ 2.1-databases/work_with_database/phones/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 from .models import Phone
 
-
-# class PhoneAdmin(admin.ModelAdmin):
-#     prepopulated_fields = {'slug': ('name',)}
-#
-#
-# admin.site.register(Phone, PhoneAdmin)
