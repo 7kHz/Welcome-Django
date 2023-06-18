c5d83ac94972d45919760ed45b2046e5934f3d8a
7kHz <guitarheroe2012@ya.ru>
2023-06-18 23:01:05 +0300

    first commit

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,25 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from model_bakery import baker
+from rest_framework.test import APIClient
+
+
+from students.models import Course
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+# @pytest.fixture
+# def course_factory():
+#     def course(*args, **kwargs):
+#         return baker.make(Course, *args, **kwargs)
+#     return course
+
+
+@pytest.mark.django_db
+def test_course():
+    client = APIClient()
+    response = client.get('/api/v1/courses/', {'name': 'Jessy'})
+    assert response.status_code == 200
