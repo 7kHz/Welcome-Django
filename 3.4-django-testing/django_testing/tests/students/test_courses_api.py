163dfcc03b16b2161693f7de198bd031a6c69f89
Maxim Uteshev <guitarheroe2012@ya.ru>
2023-06-19 17:56:14 +0300

    second commit

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -2,8 +2,7 @@ import pytest
 from model_bakery import baker
 from rest_framework.test import APIClient
 
-
-from students.models import Course
+from students.models import Course, Student
 
 
 @pytest.fixture
@@ -19,7 +18,10 @@ def client():
 
 
 @pytest.mark.django_db
-def test_course():
-    client = APIClient()
-    response = client.get('/api/v1/courses/', {'name': 'Jessy'})
-    assert response.status_code == 200
+# def test_course():
+#     client = APIClient()
+#     response = client.get('/api/v1/courses/', {'name': 'Philosophy'}, format='json')
+#     data = response.json()
+#     assert data == response
+#     assert response.status_code == 200
+#     assert response[0]['name'] == 'Philosophy'
